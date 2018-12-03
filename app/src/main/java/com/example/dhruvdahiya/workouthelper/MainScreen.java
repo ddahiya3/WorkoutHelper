8452f7d0e5eb1138d50b14e3792b6b13232923c8
Dhruv Dahiya <ddahiya3@illinois.edu>
2018-12-03 02:56:31 -0600

    new activity page

--- app/src/main/java/com/example/dhruvdahiya/workouthelper/MainScreen.java
+++ app/src/main/java/com/example/dhruvdahiya/workouthelper/MainScreen.java
@@ -2,6 +2,7 @@ package com.example.dhruvdahiya.workouthelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -10,4 +11,10 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
     }
+
+    /** Called when the user taps the ENTER button */
+    public void getResults(View view) {
+        // Do something in response to button
+    }
+
 }
