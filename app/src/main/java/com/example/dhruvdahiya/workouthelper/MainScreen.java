9070a91e75faf9c4d101aa6761c43c64d46c09cb
xjhig <xjh2@illinois.edu>
2018-12-03 04:22:01 -0600

    Buttons on both the main screen and the search screen now allow travel to each other.

--- app/src/main/java/com/example/dhruvdahiya/workouthelper/MainScreen.java
+++ app/src/main/java/com/example/dhruvdahiya/workouthelper/MainScreen.java
@@ -1,20 +1,33 @@
 package com.example.dhruvdahiya.workouthelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainScreen extends AppCompatActivity {
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSearchDisplayPage();
+            }
+        });
     }
 
     /** Called when the user taps the ENTER button */
-    public void getResults(View view) {
-        // Do something in response to button
+    public void openSearchDisplayPage() {
+        Intent intent = new Intent(this, searchresultpage.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/dhruvdahiya/workouthelper/searchresultpage.java
+++ app/src/main/java/com/example/dhruvdahiya/workouthelper/searchresultpage.java
@@ -1,13 +1,34 @@
 package com.example.dhruvdahiya.workouthelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class searchresultpage extends AppCompatActivity {
+
+    private Button searchresultpageGoBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchresultpage);
+
+        searchresultpageGoBack = (Button) findViewById(R.id.srpGoBack);
+        searchresultpageGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goBackToMain();
+            }
+        });
+    }
+
+    /**
+     * This is the Go Back button's function.
+     */
+    public void goBackToMain() {
+        Intent intent = new Intent(this, MainScreen.class);
+        startActivity(intent);
     }
 }
