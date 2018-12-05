98883f83b6d63c6dc93f5ec2be6353dcce1a3313
xjhig <xjh2@illinois.edu>
2018-12-04 21:08:02 -0600

    Testing basic api calls

--- app/src/main/java/com/example/dhruvdahiya/workouthelper/MainScreen.java
+++ app/src/main/java/com/example/dhruvdahiya/workouthelper/MainScreen.java
@@ -6,10 +6,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainScreen extends AppCompatActivity {
     private static final String LOG_TAG = MainScreen.class.getSimpleName();
-    private Button button;
+
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
 
 
     @Override
@@ -17,11 +32,14 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        mTextViewResult = findViewById(R.id.simpleTextView);
+        Button enterButton = (Button) findViewById(R.id.enter);
+        mQueue = Volley.newRequestQueue(this);
+        enterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openSearchDisplayPage();
+                //openSearchDisplayPage();
+                jsonParse();
             }
         });
     }
@@ -32,5 +50,34 @@ public class MainScreen extends AppCompatActivity {
         startActivity(intent);
         Log.d(LOG_TAG, "Button clicked!");
     }
+    public void jsonParse() {
+        String url = "https://api.myjson.com/bins/u2gbi";
 
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("result");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject result = jsonArray.getJSONObject(i);
+
+                                int id = result.getInt("id");
+
+                                mTextViewResult.append(String.valueOf(id) + "\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+
+    }
 }
--- app/src/main/java/com/example/dhruvdahiya/workouthelper/searchresultpage.java
+++ app/src/main/java/com/example/dhruvdahiya/workouthelper/searchresultpage.java
@@ -1,21 +1,21 @@
 package com.example.dhruvdahiya.workouthelper;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.util.Log;
 
 public class searchresultpage extends AppCompatActivity {
     private static final String LOG_TAG = searchresultpage.class.getSimpleName();
     private Button searchresultpageGoBack;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchresultpage);
-
         searchresultpageGoBack = (Button) findViewById(R.id.srpGoBack);
         searchresultpageGoBack.setOnClickListener(new View.OnClickListener() {
             @Override
